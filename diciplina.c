8ed4a21657dcdb522798471819e9e822f6052434
rafaeldcb <rafaeldcb@gmail.com>
2022-07-05 15:40:01 -0300

    criação dos .h e modificação da mian p/ se adaptar

---
+++ diciplina.c
@@ -0,0 +1,9 @@
+#ifndef diciplina_h
+    #define diciplina_h
+    #include "structs.h"
+    #include "structs.h"
+    
+
+    
+
+    #endif
---
+++ structs.h
@@ -0,0 +1,36 @@
+typedef struct
+{
+    char nome[20];
+    char sobrenome[50];
+    int nacionalidade;
+    char cpf[12];
+    char rg[8];
+    char passaporte[9];
+    char paisOrigem[20];
+    char dt_nasc[11];
+    int pcd;
+    char laudo[10];
+    char cep[9];
+    int proforaluno;
+    int matricula;
+    char pis;
+
+} Pessoa;
+
+typedef struct
+{
+    long int codigo;
+    char nome[20];
+    int cargaHoraria;
+    Pessoa professor;
+    Pessoa aluno[10];
+
+} Disciplina;
+
+typedef struct
+{
+    Pessoa alunos[30];
+    Pessoa professores[5];
+    Disciplina disciplina[5];
+
+} Escola;
